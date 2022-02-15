6bf725798cd9c1026bb3f6773c705009e8bb4c1f
ManfredBalvet <manfred.balvet@gmail.com>
2022-02-14 23:00:11 -0500

    Quick GUI added

--- config_for_length.py
+++ config_for_length.py
@@ -1,12 +1,12 @@
-def config_for_length(length: int):
-    mo_lm_014_0585 = 585
-    mo_lm_014_0855 = 855
-    mo_lm_014_1530 = 1530
-    mo_lm_014_2295 = 2295
-    mo_lm_014_0585_price = 232.54
-    mo_lm_014_0855_price = 292.66
-    mo_lm_014_1530_price = 357.68
-    mo_lm_014_2295_price = 539.12
+
+def config_for_length(length: int) -> str:
+    mo_lm_014 = [
+        (2295, 539.12),
+        (1530, 357.68),
+        (855, 292.66),
+        (585, 232.54)
+    ]
+    _0585 = 585
 
     closest_high = length
     closest_low = - length
@@ -14,11 +14,11 @@ def config_for_length(length: int):
     config_high = []
     config_exact = []
 
-    for w in range(length // mo_lm_014_2295 + 2):
-        for x in range(length // mo_lm_014_1530 + 2):
-            for y in range(length // mo_lm_014_0855 + 2):
-                for z in range(length // mo_lm_014_0585 + 2):
-                    possible_length = mo_lm_014_2295 * w + mo_lm_014_1530 * x + mo_lm_014_0855 * y + mo_lm_014_0585 * z
+    for w in range(length // mo_lm_014[0][0] + 2):
+        for x in range(length // mo_lm_014[1][0] + 2):
+            for y in range(length // mo_lm_014[2][0] + 2):
+                for z in range(length // mo_lm_014[3][0] + 2):
+                    possible_length = mo_lm_014[0][0] * w + mo_lm_014[1][0] * x + mo_lm_014[2][0] * y + mo_lm_014[3][0] * z
                     difference = possible_length - length
                     if difference == 0:
                         config_exact.append([w, x, y, z])
@@ -36,31 +36,39 @@ def config_for_length(length: int):
                             config_high = [[w, x, y, z]]
                         elif closest_high == difference:
                             config_high.append([w, x, y, z])
-
-    print(f"For a length of {length}mm :")
+    output = ""
     if len(config_exact) == 0:
-        if closest_high > abs(closest_low):
-            for [w, x, y, z] in config_low:
-                price = w * mo_lm_014_2295_price + x * mo_lm_014_1530_price + y * mo_lm_014_0855_price + z * mo_lm_014_0585_price
-                segments = w + x + y + z
-                print(f"{w}x MO-LM-014-2295, {x}x MO-LM-014-1530, {y}x MO-LM-014-0855, {z}x MO-LM-014-585 will be too short by {abs(closest_low)} for {segments} segments at {price}$")
-        elif abs(closest_low) > closest_high:
-            for [w, x, y, z] in config_high:
-                price = w * mo_lm_014_2295_price + x * mo_lm_014_1530_price + y * mo_lm_014_0855_price + z * mo_lm_014_0585_price
-                segments = w + x + y + z
-                print(f"{w}x MO-LM-014-2295, {x}x MO-LM-014-1530, {y}x MO-LM-014-0855, {z}x MO-LM-014-585 will be too long by {abs(closest_high)} for {segments} segments at {price}$")
-        else:
-            for [w, x, y, z] in config_low:
-                price = w * mo_lm_014_2295_price + x * mo_lm_014_1530_price + y * mo_lm_014_0855_price + z * mo_lm_014_0585_price
-                segments = w + x + y + z
-                print(f"{w}x MO-LM-014-2295, {x}x MO-LM-014-1530, {y}x MO-LM-014-0855, {z}x MO-LM-014-585 will be too short by {abs(closest_low)} for {segments} segments at {price}$")
-            for [w, x, y, z] in config_high:
-                price = w * mo_lm_014_2295_price + x * mo_lm_014_1530_price + y * mo_lm_014_0855_price + z * mo_lm_014_0585_price
-                segments = w + x + y + z
-                print(f"{w}x MO-LM-014-2295, {x}x MO-LM-014-1530, {y}x MO-LM-014-0855, {z}x MO-LM-014-585 will be too long by {abs(closest_high)} for {segments} segments at {price}$")
+        for [w, x, y, z] in config_low:
+            price = __get_price([w, x, y, z], mo_lm_014)
+            segments = w + x + y + z
+            for i, segment in enumerate([w, x, y, z]):
+                if segment > 0:
+                    output += f"{segment}x MO-LM-014-{mo_lm_014[i][0]}, "
+            output += f"will be too short by {abs(closest_low)} for {segments} segments at {price}$\n"
+
+        for [w, x, y, z] in config_high:
+            price = __get_price([w, x, y, z], mo_lm_014)
+            segments = w + x + y + z
+            for i, segment in enumerate([w, x, y, z]):
+                if segment > 0:
+                    output += f"{segment}x MO-LM-014-{mo_lm_014[i][0]}, "
+            output += f"will be too long by {abs(closest_high)} for {segments} segments at {price}$\n"
+
     else:
         for [w, x, y, z] in config_exact:
-            price = w * mo_lm_014_2295_price + x * mo_lm_014_1530_price + y * mo_lm_014_0855_price + z * mo_lm_014_0585_price
+            price = __get_price([w, x, y, z], mo_lm_014)
             segments = w + x + y + z
-            print(f"{w}x MO-LM-014-2295, {x}x MO-LM-014-1530, {y}x MO-LM-014-0855, {z}x MO-LM-014-585 will work for {segments} segments at {price}$")
+            for i, segment in enumerate([w, x, y, z]):
+                if segment > 0:
+                    output += f"{segment}x MO-LM-014-{mo_lm_014[i][0]}, "
+            output += f"will work for {segments} segments at {price}$\n"
 
+    return output
+
+
+def __get_price(config: list[int], mo_lm_014: list[tuple[int, float]]) -> int:
+    price = 0
+    for i, segments in enumerate(config):
+        price += segments * mo_lm_014[i][1]
+
+    return price
--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 from config_for_length import config_for_length
+import tkinter as tk
 
 
 def main():
-    length = int(input("What length you want to build? "))
-    config_for_length(length)
+    length = answer.get()
+    if length.isdigit():
+        output = config_for_length(int(length))
+        lbl_output["text"] = f"\nFor a {length}mm shaft you need:\n\n" + output
+    else:
+        lbl_output["text"] = "\nYour length needs to be a number"
 
 
 if __name__ == "__main__":
-    main()
+    window = tk.Tk()
+    window.title("Long Shafts Configurations")
+    window.resizable(width=True, height=True)
+
+    question = tk.Label(master=window, text="What length you want to build?", width=60)
+    answer = tk.Entry(master=window, width=30)
+    button = tk.Button(master=window, text="Calculate!", command=main)
+    lbl_output = tk.Label(master=window,)
+
+    question.grid(row=0, column=0)
+    answer.grid(row=1, column=0)
+    button.grid(row=2, column=0, sticky="ns")
+    lbl_output.grid(row=3, column=0, sticky="nw")
+
+    window.mainloop()
+
 
